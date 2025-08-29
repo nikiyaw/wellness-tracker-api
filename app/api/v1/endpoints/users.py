60267d31f69350676043dd69390858d21fc30536
Niki Yaw <ql231995@student.ubc.ca>
2025-08-29 11:25:42 -0700

    create User API endpoints

--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -1,3 +1,5 @@
+# Create a API router to handle user-related requests (creating & reading a user)
+
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from app.database.database import get_db
@@ -6,4 +8,16 @@ from app.schemas.user import UserCreate, User as UserSchema
 
 router = APIRouter()
 
- 
+@router.post("/", response_model=UserSchema, status_code=status.HTTP_201_CREATED)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = db.query(UserModel).filter(UserModel.email == user.email).first()
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    
+    hashed_password = user.password
+
+    new_user = UserModel(email=user.email, password_hash=hashed_password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
