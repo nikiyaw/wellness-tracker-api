10e276025b5a3a9394fe819b8f26f5cff26d116f
Niki Yaw <ql231995@student.ubc.ca>
2025-08-29 11:47:54 -0700

    create security utility file

---
+++ app/core/security.py
@@ -0,0 +1,13 @@
+# To handle password hasing and verification logic
+
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemas=["bcrypt"], deprecated="auto")
+
+# Hash a password using bcrypt
+def get_password_hsah(password: str) -> str:
+    return pwd_context.hash(password)
+
+# Verify a plain password against a hashed password
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
