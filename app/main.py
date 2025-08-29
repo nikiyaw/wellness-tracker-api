7d39640f761e8352b5d23efb232a6efd0f42e6dc
Niki Yaw <ql231995@student.ubc.ca>
2025-08-29 11:34:29 -0700

    integrate the User Router in main.py

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from app.api.v1.endpoints import habits, users
+
+app = FastAPI(
+    title="Personal Wellness tracker API",
+    description="A backend API for tracking personal wellness data.",
+    version="1.0.0"
+)
+
+app.include_router(habits.router, prefix ="/v1/habits", tags=["habits"])
+app.include_router(users.router, prefix="/v1/users", tags=["users"])
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Personal Wellness Tracker API"}
