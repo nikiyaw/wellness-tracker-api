f61a1ed5bfa302be97a555e67c0c74529569f2b0
Niki Yaw <ql231995@student.ubc.ca>
2025-09-02 14:25:43 -0700

    finalizing main.py file

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.api.v1.endpoints import habits, users
+from app.api.v1.endpoints import habits
 
 app = FastAPI(
     title="Personal Wellness tracker API",
@@ -7,8 +7,8 @@ app = FastAPI(
     version="1.0.0"
 )
 
+# Include the habits router. This makes all habit endpoints available.
 app.include_router(habits.router, prefix ="/v1/habits", tags=["habits"])
-app.include_router(users.router, prefix="/v1/users", tags=["users"])
 
 @app.get("/")
 def read_root():
