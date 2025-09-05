a6defba8ab7e80d0500a64a25871731c30a11bdc
Niki Yaw <nikyaw95@gmail.com>
2025-09-05 14:54:43 -0700

    pass database credentials to function directly as keyword arguments

--- alembic/env.py
+++ alembic/env.py
@@ -66,9 +66,17 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = os.getenv("DATABASE_URL")
-    if connectable is not None:
-        connectable = create_engine(connectable)
+    # Use environment variables for a more robust connection
+    db_user = os.getenv("POSTGRES_USER")
+    db_password = os.getenv("POSTGRES_PASSWORD")
+    db_host = "localhost" # It is hardcoded to localhost because the database is run in a service container
+    db_port = "5432" # It is hardcoded to 5432
+    db_name = os.getenv("POSTGRES_DB")
+
+    if db_user and db_password and db_name:
+        connectable = create_engine(
+            f"postgresql+psycopg://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+        )
     else:
         connectable = engine_from_config(
             config.get_section(config.config_ini_section, {}),
