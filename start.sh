2c82c111848269e3ed6a59fd2e493152e3fb1b2f
Niki Yaw <nikyaw95@gmail.com>
2025-09-10 20:41:14 -0700

    added render.yaml to solve psycopg issue

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ set -e
 
 # Run Alembic migrations to create/update database tables
 echo "Running database migrations..."
-alembic upgrade head
+DATABASE_URL="postgresql+psycopg://wellness_tracker_db_llvw_user:a0f0MPJZJkH2wSY5E9ncGgjR3QE0TKbN@dpg-d2togl7fte5s73aeoog0-a/wellness_tracker_db_llvw" alembic upgrade head
 
 # Start the application server
 echo "Starting the application..."
